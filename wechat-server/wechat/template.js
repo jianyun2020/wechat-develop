e726e7161dd1b08cdc47417cf808886f405ee1aa
jianyun2020 <sujianyun2020@163.com>
2021-07-28 18:27:15 +0800

    add

--- wechat-server/wechat/template.js
+++ wechat-server/wechat/template.js
@@ -10,6 +10,31 @@ module.exports = options => {
     case 'text':
       replyMessage += `<Content><![CDATA[${options.Content}]]></Content>`
       break;
+    case 'image':
+      replyMessage += `<Image><MediaId><![CDATA[${options.mediaId}]]></MediaId></Image>`
+      break;
+    case 'voice':
+      replyMessage += `  <Voice><MediaId><![CDATA[${options.mediaId}]]></MediaId></Voice>`
+      break;
+    case 'video':
+      replyMessage += `<Video>
+        <MediaId><![CDATA[${options.mediaId}]]></MediaId>
+        <Title><![CDATA[${options.title}]]></Title>
+        <Description><![CDATA[${options.description}]]></Description>
+      </Video>`
+      break;
+    case 'music':
+      replyMessage += `<Music>
+      <Title><![CDATA[${title}]]></Title>
+      <Description><![CDATA[${options.description}]]></Description>
+      <MusicUrl><![CDATA[${options.musicUrl}]]></MusicUrl>
+      <HQMusicUrl><![CDATA[${options.hqMusicUrl}]]></HQMusicUrl>
+      <ThumbMediaId><![CDATA[${options.mediaId}]]></ThumbMediaId>
+    </Music>`
+      break;
+    case 'text':
+      replyMessage += `<Content><![CDATA[${options.Content}]]></Content>`
+      break;
   
   }  
 
